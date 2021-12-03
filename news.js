d85fefa733cf84b6f24309f4a970bd303eb304a7
Ajaz <ajazqwq@gmail.com>
2021-12-03 10:48:37 +0530

    intialized the url

--- news.js
+++ news.js
@@ -20,31 +20,24 @@
 let apiKey = "b65e82c1ab1b4e768a273f36de34557d";
 let searchItem = "bitcoin";
 let country = "in";
+
 let url = `https://newsapi.org/v2/everything?q=editorials&sortBy=popularity&apiKey=${apiKey}`;
 let storyUrl = `https://newsapi.org/v2/everything?q=op-ed&apiKey=${apiKey}`;
 let sideUrl = `https://newsapi.org/v2/everything?q=sports&apiKey=${apiKey}`;
-
-let headLineUrl = `https://newsapi.org/v2/top-headlines?
-country=${country}&
-apiKey=${apiKey}`;
-
-let headLineUrl2 = `https://newsapi.org/v2/top-headlines?country=in&
-apiKey=${apiKey}`;
+let headLineUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+let headLineUrl2 = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
 
 console.log("connected");
 
 let headline_text = document.getElementById("head-text"),
-    headline_image = document.getElementById("head-img");
-let newsLaod = document.querySelector(".news-random2");
-let sideLoad = document.querySelector("#newsLoad");
+    headline_image = document.getElementById("head-img"),
+    newsLaod = document.querySelector(".news-random2"),
+    sideLoad = document.querySelector("#newsLoad"),
+    news_slides = document.getElementsByClassName("newsSlide"),
+    Nname = document.getElementById("name"),
+    side_news = document.getElementsByClassName("newsChild"),
+    author = document.getElementsByClassName("author");
 
-// console.log(headline_image);
-// headline_image.src = "mike.jpg";
-
-let Nname = document.getElementById("name");
-let side_news = document.getElementsByClassName("newsChild");
-
-let i = 0;
 let loadHead = () => {
     fetch(headLineUrl2)
         .then((response) => response.json())
@@ -52,52 +45,15 @@ let loadHead = () => {
             (headline_text.innerText = data.articles[0].title),
                 (headline_image.src = data.articles[0].urlToImage),
                 (headline_text.href = data.articles[0].url);
-            for (key of side_news) {
-                (key.firstElementChild.src = data.articles[i].urlToImage),
-                    (key.lastElementChild.href = data.articles[i].url),
-                    (key.lastElementChild.innerText = data.articles[i].title);
-                i++;
-            }
         });
 };
 
-let author = document.getElementsByClassName("author");
-// console.log(author[0]);
-// loadHead();
-let j = 0;
 let loadnews = () => {
     let html = "";
 
     fetch(url)
         .then((response) => response.json())
-        .then((data) => {
-            // console.log(data.articles);
-            // for(let r in data.articles){
-            // console.log(data.articles[i].author != null);
-            // console.log(data.articles[r].urlToImage != null);}
-            // for (let item of data.articles) {
-            //     if (item.urlToImage != null) {
-            //         html += `<div class="news-random news-random-style"><img src="${item.urlToImage}" alt=""><div class="news-roundup"><a href="${item.url}" target="_blank" class="heading-3">${item.title}</a></div></div>`;
-            //     }
-            // }
-            for (let york of news_slides) {
-                if (
-                    data.articles[i].urlToImage != null &&
-                    data.articles[i].author != null
-                ) {
-                    console.log(data.articles[i]);
-                    (york.firstElementChild.src = data.articles[i].urlToImage),
-                        (york.lastElementChild.innerText =
-                            data.articles[i].author),
-                        (author[i].href = data.articles[i].url),
-                        (author[i].innerText = data.articles[i].title);
-
-                    // j++;
-                }
-                i++;
-            }
-            // newsLaod.innerHTML = html;
-        });
+        .then((data) => {});
 };
 
 let script = "";
@@ -124,14 +80,24 @@ let loadSideStories = () => {
             for (let item of data.articles) {
                 // console.log(item);
                 if (item.urlToImage != null && item.title != null) {
-                    sideScript += `<div class="news-random "><img src="${item.urlToImage}" alt=""><div class="news-roundup"><h2 class="heading-3">${item.title}</h2></div></div>`;
+                    sideScript += `<div class="news-random "><img src="${item.urlToImage}" alt=""><div class="news-roundup"><a href="${item.url}" target="_blank" class="heading-3">${item.title}</a></div></div>`;
                 }
             }
             sideLoad.innerHTML = sideScript;
         });
 };
-loadStories();
-loadSideStories();
-let news_slides = document.getElementsByClassName("newsSlide");
+
+// loadStories();
+// loadSideStories();
+// loadHeadline();
+// loadBreaking();
+// loadEditorials();
+// loadBynational();
+// loadByinternational();
+// loadBycovid();
+// loadByBusiness();
+// loadBypolitics();
+// loadBySports();
+
 
 Nname.addEventListener("click", loadnews);
